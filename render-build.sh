b950d3adeecd6e4a992c41d95b204ae13c76d1fb
Koushik Biswas <144088942+Koush98@users.noreply.github.com>
2025-03-25 15:46:06 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Update package list
+apt-get update
+
+# Install ODBC Driver 18 for SQL Server
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
+
+# Install Python dependencies
+pip install -r requirements.txt
